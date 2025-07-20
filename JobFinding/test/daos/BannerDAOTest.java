85783f4b181cff195a8740246095fb910dedba90
kientm0810 <chubohamhoc@gmail.com>
2025-07-20 12:53:12 +0700

    Change sql. Kien.

---
+++ JobFinding/test/daos/BannerDAOTest.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package daos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author andin
+ */
+public class BannerDAOTest {
+    
+    public BannerDAOTest() {
+    }
+
+    @Test
+    public void testGetAllBanners() {
+    }
+
+    @Test
+    public void testGetBannersByAdminId() {
+    }
+
+    @Test
+    public void testGetBannerById() {
+    }
+
+    @Test
+    public void testInsertBanner() {
+    }
+
+    @Test
+    public void testUpdateBanner() {
+    }
+
+    @Test
+    public void testDeleteBanner() {
+    }
+
+    @Test
+    public void testGetTotalBanners() {
+    }
+
+    @Test
+    public void testGetTotalBannersByTitle() {
+    }
+
+    @Test
+    public void testGetAllBannersWithPagingAndSorting() {
+    }
+
+    @Test
+    public void testSearchBannersByTitleWithPaging() {
+    }
+    
+}
